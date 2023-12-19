10d3ba82bbf8a0ecd6bd05628ea23db5c624eebc
Amine Akrichi <amineakrichi21@gmail.com>
2023-12-19 21:12:34 +0100

    First Commit

---
+++ src/main/java/org/example/sunnydriveapi/model/Role.java
@@ -0,0 +1,6 @@
+package org.example.sunnydriveapi.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/org/example/sunnydriveapi/model/UserEntity.java
+++ src/main/java/org/example/sunnydriveapi/model/UserEntity.java
@@ -1,4 +1,25 @@
 package org.example.sunnydriveapi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Role role;
 }
